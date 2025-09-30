603a896b8acd8f8a15a160e7e0dfe67561613336
ponrajvishnu <ponrajsm@gmail.com>
2025-09-30 12:40:00 +0530

    change port - second commit

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ app.use('/api/users', userRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log('Server listening on port 5000');
+    console.log(`Server listening on port ${port}`);
 })
