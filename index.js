67f7297b85b4d64b7f5e144fd96e1b3d596defd9
ponrajvishnu <ponrajsm@gmail.com>
2025-09-30 12:23:06 +0530

    change port to constant

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use('/api/users', userRouter);
 // })
 
 
-
-app.listen('5000', () => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
     console.log('Server listening on port 5000');
 })
